5cd9217fe8c2908d7f5800fcd7222b922ce54018
axelzarraa <axelzarra47@gmail.com>
2026-06-28 18:14:38 +0700

    Add prisma ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-
-const adapter = new PrismaMariaDb({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "inventra_db",
-});
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
